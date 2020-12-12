c3580e38e3840f71909531f99e26fae64145131a
msi- <mrtkrt96@gmail.com>
2020-12-12 03:08:07 +0300

    css added

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler,RotatingFileHandler
 import os
@@ -13,6 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
